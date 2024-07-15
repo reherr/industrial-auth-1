616da526757099ac84a57e8686384ebcbde5ed93
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-07-15 16:45:52 +0000

    added security to roots and photos#destroy

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -50,11 +50,15 @@ class PhotosController < ApplicationController
 
   # DELETE /photos/1 or /photos/1.json
   def destroy
+      if current_user == @photo.owner
     @photo.destroy
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Photo was successfully destroyed." }
       format.json { head :no_content }
     end
+      else
+    redirect_back(fallback_location: root_url, notice: "Nice try, but that is not your photo.")
+      end
   end
 
   private
